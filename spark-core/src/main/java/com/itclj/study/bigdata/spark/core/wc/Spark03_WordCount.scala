4dc10ec5cb8571f5c2a14ed0934f0a3f99ccaf93
clj198606061111@163.com <clj-1986>
2022-11-27 22:54:37 +0800

    log4j 日志配置

--- spark-core/src/main/java/com/itclj/study/bigdata/spark/core/wc/Spark03_WordCount.scala
+++ spark-core/src/main/java/com/itclj/study/bigdata/spark/core/wc/Spark03_WordCount.scala
@@ -9,7 +9,7 @@ import org.apache.spark.{SparkConf, SparkContext}
  * var是一个可变变量
  * val是一个只读变量
  */
-object Spark02_WordCount {
+object Spark03_WordCount {
   def main(args: Array[String]): Unit = {
 
     val sparkConf = new SparkConf().setMaster("local").setAppName("ItcljWorldCount");
@@ -18,17 +18,11 @@ object Spark02_WordCount {
     val words: RDD[String] = lines.flatMap(_.split(" "))
     //转为map，方便统计
     val worldToOne = words.map(world => (world, 1))
-    //按world分组
-    val wordGroup = worldToOne.groupBy(t => t._1)
-    val wordToCount = wordGroup.map {
-      case (word, list) => {
-        list.reduce(
-          (t1, t2) => {
-            (t1._1, t1._2 + t2._2)
-          }
-        )
-      }
-    }
+
+    //spark提供了更多的功能，分组和聚合用一个方法实现
+    //reduceByKey:相同的key的数据，可以对value进行reduce聚合
+    val wordToCount = worldToOne.reduceByKey((x,y)=>{x+y})
+    //worldToOne.reduceByKey(_+_)//或者这样写，自解原则
 
     //5.将转换结构，采集到控制台打印
     val array = wordToCount.collect()
